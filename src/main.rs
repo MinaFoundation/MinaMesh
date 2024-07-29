40d281e50cddd6e932ad5a517ff223c4110d4c85
Harry Solovay <harrysolovay@gmail.com>
2024-07-29 09:55:22 -0400

    Scaffold CLI and Start Command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(
+  name = "mina-mesh",
+  version,
+  about = "A Mesh-compliant Server for Mina",
+  propagate_version = true,
+  author = "Mina Foundation"
+)]
+struct MinaMeshArgs {
+  #[arg(long, short = 'v', default_value = "false", global = true)]
+  verbose: bool,
+  #[command(subcommand)]
+  command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+  #[command(about = "Start the Mina Mesh Server")]
+  Start {
+    #[arg(long, short = 'p', default_value = "8686")]
+    port: u16,
+  },
+}
+
 fn main() {
-  println!("Hello, Mina Mesh!");
+  let args = MinaMeshArgs::parse();
+  println!("Hello, Mina Mesh!, {:?}", args);
 }
