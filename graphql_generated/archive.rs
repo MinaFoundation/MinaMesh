8556ef443a2d809f2d19427febe8cea6c6e6bfce
Harry Solovay <harrysolovay@pm.me>
2024-08-19 08:52:30 -0400

    Scaffold Balance Response Usage

--- graphql_generated/archive.rs
+++ graphql_generated/archive.rs
@@ -1,3 +1,30 @@
 #[cynic::schema("archive")]
 mod schema {}
 
+#[derive(cynic::QueryVariables, Debug)]
+pub struct SomeDocVariables<'a> {
+    pub address: &'a str,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(graphql_type = "Query", variables = "SomeDocVariables", schema = "archive")]
+pub struct SomeDoc {
+    #[arguments(input: { address: $address, from: 0, to: 1000 })]
+    pub events: Vec<Option<EventOutput>>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(schema = "archive")]
+pub struct EventOutput {
+    pub block_info: Option<BlockInfo>,
+}
+
+#[derive(cynic::QueryFragment, Debug)]
+#[cynic(schema = "archive")]
+pub struct BlockInfo {
+    pub chain_status: String,
+    pub timestamp: String,
+    pub state_hash: String,
+    pub global_slot_since_hardfork: i32,
+}
+
--- handlers/account.rs
+++ handlers/account.rs
@@ -1,11 +1,13 @@
 use super::Context;
-use crate::graphql_generated::mina::{BalanceQuery, BalanceQueryVariables, PublicKey};
+use crate::graphql_generated::archive::{SomeDoc, SomeDocVariables};
+use crate::graphql_generated::mina::{Account, BalanceQuery, BalanceQueryVariables, PublicKey};
 use anyhow::Result;
 use cynic::{http::ReqwestExt, QueryBuilder};
+use mesh::models::{AccountBalanceResponse, Amount, BlockIdentifier, Currency};
 use serde::Serialize;
 
 /// https://github.com/MinaProtocol/mina/blob/985eda49bdfabc046ef9001d3c406e688bc7ec45/src/app/rosetta/lib/account.ml#L11
-async fn balance(public_key: String) -> Result<()> {
+async fn balance(public_key: String) -> Result<AccountBalanceResponse> {
   let context = Context::from_env()?;
   let operation = BalanceQuery::build(BalanceQueryVariables {
     public_key: PublicKey(public_key),
@@ -16,16 +18,23 @@ async fn balance(public_key: String) -> Result<()> {
     .post(context.config.mina_proxy_url)
     .run_graphql(operation)
     .await?;
-  if let Some(v) = result.data {
-    println!("{:?}", v);
-    if let Some(w) = v.account {
-      println!("{:?}", w.balance.total);
-    }
+  if let Some(BalanceQuery {
+    account: Some(Account { balance, .. }),
+  }) = result.data
+  {
+    println!("Balance: {:?}", balance);
+    return Ok(AccountBalanceResponse {
+      balances: vec![],
+      block_identifier: Box::new(BlockIdentifier {
+        hash: "".into(),
+        index: 1,
+      }),
+      metadata: None,
+    });
+  } else if result.errors.is_some() {
+    anyhow::bail!("Failed to get balance: {:?}", result.errors);
   }
-  if result.errors.is_some() {
-    return Err(anyhow::anyhow!("Failed to get balance"));
-  }
-  Ok(())
+  anyhow::bail!("Failed to get balance");
 }
 
 fn coins() {}
@@ -36,7 +45,7 @@ mod test {
 
   #[tokio::test]
   async fn first() {
-    let x = balance("TODO".into()).await;
+    let x = balance("".into()).await;
     println!("{:?}", x);
   }
 }
