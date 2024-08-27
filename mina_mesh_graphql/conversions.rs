b7c0b407a9b9118dcfcc27c02ee15f59e59b09fa
Harry Solovay <harrysolovay@gmail.com>
2024-08-27 15:28:11 -0400

    cleanup

---
+++ mina_mesh_graphql/conversions.rs
@@ -0,0 +1,20 @@
+use super::SyncStatus;
+use mesh::models::SyncStatus as MeshSyncStatus;
+
+impl Into<MeshSyncStatus> for SyncStatus {
+  fn into(self) -> MeshSyncStatus {
+    let (stage, synced) = match self {
+      Self::Bootstrap => ("Bootstrap", false),
+      Self::Catchup => ("Catchup", false),
+      Self::Connecting => ("Connecting", false),
+      Self::Listening => ("Listening", false),
+      Self::Offline => ("Offline", false),
+      Self::Synced => ("Synced", true),
+    };
+    MeshSyncStatus {
+      stage: Some(stage.to_string()),
+      synced: Some(synced),
+      ..Default::default()
+    }
+  }
+}
