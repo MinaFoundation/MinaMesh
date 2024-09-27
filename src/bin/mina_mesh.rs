419bb0c772ee0b36eb884d8485b444eb2a4cd3a1
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-09-27 11:34:11 +0200

    Fix missing .env error

--- src/bin/mina_mesh.rs
+++ src/bin/mina_mesh.rs
@@ -14,7 +14,7 @@ enum Command {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  dotenv::dotenv()?;
+  dotenv::dotenv().ok();
   match Command::parse() {
     Command::Serve(cmd) => cmd.run().await,
     Command::FetchGenesisBlockIdentifier(cmd) => cmd.run().await,
