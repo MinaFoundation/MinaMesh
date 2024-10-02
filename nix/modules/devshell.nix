8d19c46478cc21420f9666e89b8c878adef80ac9
João Santos Reis <joaosreis@outlook.pt>
2024-10-02 13:57:27 +0100

    Add nix support (#31)

---
+++ nix/modules/devshell.nix
@@ -0,0 +1,21 @@
+{ inputs, ... }:
+{
+  perSystem = { config, self', pkgs, lib, ... }: {
+    devShells.default = pkgs.mkShell {
+      name = "rust-nix-mina_mesh-shell";
+      inputsFrom = [
+        self'.devShells.rust
+        config.treefmt.build.devShell
+      ];
+      nativeBuildInputs = with pkgs; [
+        sqlx-cli
+      ];
+      packages = with pkgs; [
+        nixd # Nix language server
+        nixfmt # Nix formatter
+        cargo-watch
+        config.process-compose.cargo-doc-live.outputs.package
+      ];
+    };
+  };
+}
