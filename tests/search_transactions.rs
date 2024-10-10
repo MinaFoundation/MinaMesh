9c9172c4781fa5504b81bb924198843e9fa9e16f
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-10-10 12:21:03 +0200

    add tests

--- tests/search_transactions.rs
+++ tests/search_transactions.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use insta::assert_debug_snapshot;
 use mina_mesh::{
-  models::{NetworkIdentifier, SearchTransactionsRequest},
+  models::{NetworkIdentifier, SearchTransactionsRequest, TransactionIdentifier},
   MinaMeshConfig,
 };
 
@@ -16,6 +16,48 @@ async fn search_transactions_specified() -> Result<()> {
     // cspell:enable
     limit: Some(5),
     offset: Some(0),
+    ..Default::default()
+  };
+
+  let response = mina_mesh.search_transactions(request).await;
+
+  assert!(response.is_ok());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn search_transactions_failed() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+
+  let request = SearchTransactionsRequest {
+    network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "mainnet".to_string())),
+    max_block: Some(44),
+    status: Some("failed".to_string()),
+    limit: Some(5),
+    ..Default::default()
+  };
+
+  let response = mina_mesh.search_transactions(request).await;
+
+  assert!(response.is_ok());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn search_transactions_internal_command() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+
+  let request = SearchTransactionsRequest {
+    network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "mainnet".to_string())),
+    max_block: Some(44),
+    // cspell:disable
+    transaction_identifier: Some(Box::new(TransactionIdentifier::new(
+      "CkpZZWqdA87JmPxHA5NmFEQ3qh7pUmqXi9GBWzf4pADtPEHQAeH7M".to_string(),
+    ))),
+    // cspell:enable
+    limit: Some(5),
     ..Default::default()
   };
 
