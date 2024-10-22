b4b4610de1a0ed1ebce4e9b49ef68f05ed4f7c63
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-10-22 11:32:28 +0200

    adjust pg_wait.ts

--- tasks/pg_wait.ts
+++ tasks/pg_wait.ts
@@ -6,7 +6,6 @@ import { assertExists } from "@std/assert"
 
 const connectionString = Deno.env.get("DATABASE_URL")
 assertExists(connectionString)
-const client = new pg.Client({ connectionString })
 
 const MAX_RETRIES = 200
 const RETRY_INTERVAL_MS = 10_000
@@ -16,21 +15,29 @@ let attempts = 0
 
 while (!connected && attempts < MAX_RETRIES) {
   try {
+    const client = new pg.Client({ connectionString })
     await client.connect()
     connected = true
+    await client.end()
   } catch (err: unknown) {
     if (err instanceof Error && err.message.startsWith("Client has already been connected.")) {
       connected = true
       break
     }
+
     attempts++
-    console.log("Waiting for database to be ready.")
-    console.error(err)
+    console.log(`Attempt ${attempts} failed. Waiting for database to be ready...`)
+
+    if (attempts >= MAX_RETRIES) {
+      console.error("Max retries reached. Could not connect to the database.")
+    }
+
     await new Promise((resolve) => setTimeout(resolve, RETRY_INTERVAL_MS))
   }
 }
 
 if (connected) {
-  await client.end()
   console.log("Database ready at", connectionString)
-} else throw 0
+} else {
+  Deno.exit(1)
+}
