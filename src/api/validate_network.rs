03504ccb3bc04a9751693ab63638995afd9e22e4
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-11-21 18:18:26 +0100

    introduce dashmap cache for storing network_id

--- src/api/validate_network.rs
+++ src/api/validate_network.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use anyhow::Result;
 use coinbase_mesh::models::NetworkIdentifier;
 use cynic::QueryBuilder;
@@ -8,10 +10,35 @@ impl MinaMesh {
   // Validate that the network identifier matches the network id of the GraphQL
   // server
   pub async fn validate_network(&self, network_identifier: &NetworkIdentifier) -> Result<(), MinaMeshError> {
+    let cache_key = "network_id".to_string();
+
+    // Step 1: Check the cache
+    if let Some(cached_entry) = self.cache.get(&cache_key) {
+      let (cached_network_id, timestamp) = &*cached_entry;
+      if timestamp.elapsed() < self.cache_ttl {
+        return self.compare_network_ids(cached_network_id, network_identifier);
+      }
+    }
+
+    // Step 2: Fetch from GraphQL if cache is empty or expired
     let QueryNetworkId { network_id } = self.graphql_client.send(QueryNetworkId::build(())).await?;
+
+    // Step 3: Update the cache
+    self.cache.insert(cache_key, (network_id.clone(), Instant::now()));
+
+    // Step 4: Compare the fetched value with the expected one
+    self.compare_network_ids(&network_id, network_identifier)
+  }
+
+  fn compare_network_ids(
+    &self,
+    fetched_network_id: &str,
+    network_identifier: &NetworkIdentifier,
+  ) -> Result<(), MinaMeshError> {
     let expected_network_id = format!("{}:{}", network_identifier.blockchain, network_identifier.network);
-    if network_id != expected_network_id {
-      Err(MinaMeshError::NetworkDne(expected_network_id, network_id))
+
+    if fetched_network_id != expected_network_id {
+      Err(MinaMeshError::NetworkDne(expected_network_id, fetched_network_id.to_string()))
     } else {
       Ok(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -9,20 +9,24 @@ mod sql_to_mesh;
 mod types;
 mod util;
 
+use std::time::{Duration, Instant};
+
 pub use coinbase_mesh::models;
 use coinbase_mesh::models::BlockIdentifier;
 pub use commands::*;
 pub use config::*;
+use dashmap::DashMap;
 pub use error::*;
 use graphql::GraphQLClient;
 pub use operation::*;
 use sqlx::PgPool;
 pub use types::*;
-
 #[derive(Debug)]
 pub struct MinaMesh {
   pub graphql_client: GraphQLClient,
   pub pg_pool: PgPool,
   pub genesis_block_identifier: BlockIdentifier,
   pub search_tx_optimized: bool,
+  pub cache: DashMap<String, (String, Instant)>, // Cache for network_id or other reusable data
+  pub cache_ttl: Duration,
 }
