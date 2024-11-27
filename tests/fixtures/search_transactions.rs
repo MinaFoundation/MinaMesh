db746397c25e599022318987f09a85c3eafbab92
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-11-27 22:58:26 +0100

    more search tx fixtures

--- tests/fixtures/search_transactions.rs
+++ tests/fixtures/search_transactions.rs
@@ -1,13 +1,32 @@
-use mina_mesh::models::{NetworkIdentifier, SearchTransactionsRequest};
+use mina_mesh::models::{NetworkIdentifier, SearchTransactionsRequest, TransactionIdentifier};
 
 use super::CompareGroup;
 
 pub fn search_transactions<'a>() -> CompareGroup<'a> {
-  ("/search/transactions", vec![Box::new(SearchTransactionsRequest {
-    network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "devnet".to_string())),
-    address: Some("B62qkd6yYALkQMq2SFd5B57bJbGBMA2QuGtLPMzRhhnvexRtVRycZWP".to_string()),
-    limit: Some(5),
-    offset: Some(0),
-    ..Default::default()
-  })])
+  ("/search/transactions", vec![
+    Box::new(SearchTransactionsRequest {
+      network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "devnet".to_string())),
+      address: Some("B62qkd6yYALkQMq2SFd5B57bJbGBMA2QuGtLPMzRhhnvexRtVRycZWP".to_string()),
+      limit: Some(5),
+      offset: Some(0),
+      ..Default::default()
+    }),
+    Box::new(SearchTransactionsRequest {
+      network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "devnet".to_string())),
+      max_block: Some(44),
+      status: Some("failed".to_string()),
+      limit: Some(5),
+      ..Default::default()
+    }),
+    Box::new(SearchTransactionsRequest {
+      network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "devnet".to_string())),
+      max_block: Some(44),
+      transaction_identifier: Some(Box::new(TransactionIdentifier::new(
+        // cspell:disable-next-line
+        "CkpYcKc2oGs8JUd4tmdGBsZXQCQVkayuyffEjrNWctX5Wuad3vVNe".to_string(),
+      ))),
+      limit: Some(5),
+      ..Default::default()
+    }),
+  ])
 }
