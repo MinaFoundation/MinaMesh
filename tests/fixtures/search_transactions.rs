28609687740afdeb314834f147cd7269ac963fa4
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-11-28 12:29:35 +0100

    search tx zkapp fixture

--- tests/fixtures/search_transactions.rs
+++ tests/fixtures/search_transactions.rs
@@ -28,5 +28,14 @@ pub fn search_transactions<'a>() -> CompareGroup<'a> {
       limit: Some(5),
       ..Default::default()
     }),
+    Box::new(SearchTransactionsRequest {
+      network_identifier: Box::new(NetworkIdentifier::new("mina".to_string(), "devnet".to_string())),
+      transaction_identifier: Some(Box::new(TransactionIdentifier::new(
+        // cspell:disable-next-line
+        "5JvFoEyvuPu9zmi4bDGbhqsakre2SPQU1KKbeh2Lk5uC9eYrc2h2".to_string(),
+      ))),
+      limit: Some(1),
+      ..Default::default()
+    }),
   ])
 }
