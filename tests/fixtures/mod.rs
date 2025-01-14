b28f1b9ead5c070c836fb78343c9d7bbf095bc06
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-01-14 13:19:02 +0100

    comparison testing

--- tests/fixtures/mod.rs
+++ tests/fixtures/mod.rs
@@ -2,12 +2,14 @@ use erased_serde::Serialize as ErasedSerialize;
 
 mod account_balance;
 mod block;
+mod construction_derive;
 mod mempool;
 mod network;
 mod search_transactions;
 
 pub use account_balance::*;
 pub use block::*;
+pub use construction_derive::*;
 pub use mempool::*;
 pub use network::*;
 pub use search_transactions::*;
