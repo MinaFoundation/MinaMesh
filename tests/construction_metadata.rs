ff46f26853f5e0d926cf4c289d272d7742d23014
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-01-22 14:42:20 +0100

    functional testing

---
+++ tests/construction_metadata.rs
@@ -0,0 +1,160 @@
+use anyhow::Result;
+use insta::assert_debug_snapshot;
+use mina_mesh::{models::ConstructionMetadataRequest, test::network_id, MinaMeshConfig};
+
+#[tokio::test]
+async fn construction_metadata_ok() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    options: Some(serde_json::json!({
+      // cspell:disable
+      "sender": "B62qkd6yYALkQMq2SFd5B57bJbGBMA2QuGtLPMzRhhnvexRtVRycZWP",
+      "receiver": "B62qnXy1f75qq8c6HS2Am88Gk6UyvTHK3iSYh4Hb3nD6DS2eS6wZ4or",
+      // cspell:enable
+      "token_id": "1",
+      "valid_until": "20000",
+      "memo": "test transaction"
+    })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_ok());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_acct_not_found() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    options: Some(serde_json::json!({
+      // cspell:disable
+      "sender": "B62qpLST3UC1rpVT6SHfB7wqW2iQgiopFAGfrcovPgLjgfpDUN2LLeg",
+      "receiver": "B62qnXy1f75qq8c6HS2Am88Gk6UyvTHK3iSYh4Hb3nD6DS2eS6wZ4or",
+      // cspell:enable
+      "token_id": "1",
+      "valid_until": "20000",
+      "memo": "test transaction"
+    })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_empty() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest::new(network_id());
+  let response = mina_mesh.construction_metadata(request).await;
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_missing_sender() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    // cspell:disable-next-line
+    options: Some(serde_json::json!({ "receiver": "B62qnXy1f75qq8c6HS2Am88Gk6UyvTHK3iSYh4Hb3nD6DS2eS6wZ4or" })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_missing_receiver() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    // cspell:disable-next-line
+    options: Some(serde_json::json!({ "sender": "B62qnXy1f75qq8c6HS2Am88Gk6UyvTHK3iSYh4Hb3nD6DS2eS6wZ4or" })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_missing_token_id() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    options: Some(serde_json::json!({
+      // cspell:disable
+      "sender": "B62qnXy1f75qq8c6HS2Am88Gk6UyvTHK3iSYh4Hb3nD6DS2eS6wZ4or",
+      "receiver": "B62qpLST3UC1rpVT6SHfB7wqW2iQgiopFAGfrcovPgLjgfpDUN2LLeg",
+      // cspell:enable
+    })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_invalid_sender() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    options: Some(serde_json::json!({
+      "sender": "invalid",
+      // cspell:disable-next-line
+      "receiver": "B62qpLST3UC1rpVT6SHfB7wqW2iQgiopFAGfrcovPgLjgfpDUN2LLeg",
+      "token_id": "1",
+    })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
+
+#[tokio::test]
+async fn construction_metadata_invalid_receiver() -> Result<()> {
+  let mina_mesh = MinaMeshConfig::from_env().to_mina_mesh().await?;
+  let request = ConstructionMetadataRequest {
+    network_identifier: network_id().into(),
+    options: Some(serde_json::json!({
+      // cspell:disable-next-line
+      "sender": "B62qpLST3UC1rpVT6SHfB7wqW2iQgiopFAGfrcovPgLjgfpDUN2LLeg",
+      "receiver": "invalid",
+      "token_id": "1",
+    })),
+    public_keys: None,
+  };
+
+  let response = mina_mesh.construction_metadata(request).await;
+
+  assert!(response.is_err());
+  assert_debug_snapshot!(response);
+  Ok(())
+}
