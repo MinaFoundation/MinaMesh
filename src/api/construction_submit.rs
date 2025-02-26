28ba3dc8cc1175f9336b02fae6772fd82a02db56
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-02-26 11:25:23 +0100

    initial send_payment

--- src/api/construction_submit.rs
+++ src/api/construction_submit.rs
@@ -1,12 +1,77 @@
 use anyhow::Result;
 use coinbase_mesh::models::{ConstructionSubmitRequest, TransactionIdentifier};
+use cynic::MutationBuilder;
 
-use crate::MinaMesh;
+use crate::{
+  graphql::{SendPayment, SendPaymentVariables},
+  MinaMesh, MinaMeshError, Payment, TransactionSigned,
+};
 
 /// https://github.com/MinaProtocol/mina/blob/985eda49bdfabc046ef9001d3c406e688bc7ec45/src/app/rosetta/lib/construction.ml#L849
 impl MinaMesh {
-  pub async fn construction_submit(&self, request: ConstructionSubmitRequest) -> Result<TransactionIdentifier> {
+  pub async fn construction_submit(
+    &self,
+    request: ConstructionSubmitRequest,
+  ) -> Result<TransactionIdentifier, MinaMeshError> {
     self.validate_network(&request.network_identifier).await?;
-    Ok(TransactionIdentifier::new("".to_string()))
+
+    let signed_transaction = TransactionSigned::from_json_string(&request.signed_transaction)?;
+    if signed_transaction.payment.is_some() && signed_transaction.stake_delegation.is_some() {
+      return Err(MinaMeshError::JsonParse(Some(
+        "Signed transaction must have one of: payment, stake_delegation".to_string(),
+      )));
+    }
+
+    if signed_transaction.payment.is_some() {
+      tracing::debug!("Payment transaction");
+      let payment = signed_transaction.payment.unwrap();
+      let hash = self.send_payment(payment, &signed_transaction.signature).await?;
+      Ok(TransactionIdentifier::new(hash))
+    } else if signed_transaction.stake_delegation.is_some() {
+      tracing::debug!("Stake delegation transaction");
+      Ok(TransactionIdentifier::new("".to_string()))
+    } else {
+      tracing::debug!("Signed transaction missing payment or stake delegation");
+      return Err(MinaMeshError::JsonParse(Some("Signed transaction missing payment or stake delegation".to_string())));
+    }
+  }
+
+  async fn send_payment(&self, payment: Payment, signature: &str) -> Result<String, MinaMeshError> {
+    let variables = SendPaymentVariables {
+      amount: payment.amount.into(),
+      fee: payment.fee.into(),
+      from: payment.from.into(),
+      to: payment.to.into(),
+      nonce: payment.nonce.into(),
+      valid_until: payment.valid_until.map(|v| v.into()),
+      memo: payment.memo.as_deref(),
+      signature,
+    };
+
+    let response = self.graphql_client.send(SendPayment::build(variables)).await;
+
+    match response {
+      Ok(response) => Ok(response.send_payment.payment.hash.0),
+      Err(err) => Err(self.map_error(err).await),
+    }
+  }
+
+  async fn map_error(&self, err: MinaMeshError) -> MinaMeshError {
+    match err {
+      MinaMeshError::GraphqlMinaQuery(err) => {
+        if err.contains("Couldn't infer nonce") {
+          MinaMeshError::TransactionSubmitNoSender(err)
+        } else if err.contains("less than the minimum fee") {
+          MinaMeshError::TransactionSubmitFeeSmall(err)
+        } else if err.contains("Invalid_signature") {
+          MinaMeshError::TransactionSubmitInvalidSignature(err)
+        } else if err.contains("below minimum_nonce") {
+          MinaMeshError::TransactionSubmitBadNonce(err)
+        } else {
+          MinaMeshError::GraphqlMinaQuery(err)
+        }
+      }
+      _ => err,
+    }
   }
 }
