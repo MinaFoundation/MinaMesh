9691d9d119ed118c300e9705a332c1d48e2a10a7
David Lehuby <david.lehuby@minaprotocol.com>
2025-03-03 16:34:00 +1000

    WIP

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -17,8 +17,8 @@ initialize_postgres() {
 
 # Function to create PostgreSQL user and database
 create_postgres_user_and_db() {
-    sudo -u postgres psql --command "CREATE USER ${POSTGRES_USERNAME} WITH SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';" > /dev/null 2>&1
-    sudo -u postgres createdb -O "${POSTGRES_USERNAME}" "${POSTGRES_DBNAME}" > /dev/null 2>&1
+    sudo -u postgres psql --command "CREATE USER ${POSTGRES_USER} WITH SUPERUSER PASSWORD '${POSTGRES_PASSWORD}';" > /dev/null 2>&1
+    sudo -u postgres createdb -O "${POSTGRES_USER}" "${POSTGRES_DBNAME}" > /dev/null 2>&1
 }
 
 # Function to find and download the latest available archive dump
