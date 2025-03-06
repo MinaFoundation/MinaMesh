80370198f15abc7e95668d1bd73d3d4854dd3ce1
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-03-06 15:47:29 +0100

    initial scaffolding

--- src/api/construction_hash.rs
+++ src/api/construction_hash.rs
@@ -1,12 +1,30 @@
 use anyhow::Result;
-use coinbase_mesh::models::{ConstructionHashRequest, TransactionIdentifier};
+use coinbase_mesh::models::{ConstructionHashRequest, TransactionIdentifier, TransactionIdentifierResponse};
 
-use crate::MinaMesh;
+use crate::{MinaMesh, MinaMeshError, TransactionSigned};
 
 /// https://github.com/MinaProtocol/mina/blob/985eda49bdfabc046ef9001d3c406e688bc7ec45/src/app/rosetta/lib/construction.ml#L786
 impl MinaMesh {
-  pub async fn construction_hash(&self, request: ConstructionHashRequest) -> Result<TransactionIdentifier> {
+  pub async fn construction_hash(
+    &self,
+    request: ConstructionHashRequest,
+  ) -> Result<TransactionIdentifierResponse, MinaMeshError> {
     self.validate_network(&request.network_identifier).await?;
-    Ok(TransactionIdentifier::new("".to_string()))
+
+    let tx: TransactionSigned = TransactionSigned::from_json_string(&request.signed_transaction)
+      .map_err(|_| MinaMeshError::JsonParse(Some("Failed to parse signed transaction".to_string())))?;
+
+    let tx_hash = self.hash_signed_transaction(&tx)?;
+
+    Ok(TransactionIdentifierResponse::new(TransactionIdentifier::new(tx_hash)))
+  }
+
+  fn hash_signed_transaction(&self, tx: &TransactionSigned) -> Result<String, MinaMeshError> {
+    let serialized_tx = serde_json::to_string(tx)
+      .map_err(|_| MinaMeshError::JsonParse(Some("Failed to serialize signed transaction".to_string())))?;
+
+    let hash_b58 = bs58::encode(serialized_tx).into_string();
+
+    Ok(hash_b58)
   }
 }
