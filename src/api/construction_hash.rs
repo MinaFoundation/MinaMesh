c443350d492fd0afb0425517c2da7df289e13b13
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-03-11 10:44:22 +0100

    make user_command_payload

--- src/api/construction_hash.rs
+++ src/api/construction_hash.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use coinbase_mesh::models::{ConstructionHashRequest, TransactionIdentifier, TransactionIdentifierResponse};
+use mina_signer::{pubkey::PubKeyError, PubKey};
 
-use crate::{MinaMesh, MinaMeshError, TransactionSigned};
+use crate::{generate_operations_user_command, MinaMesh, MinaMeshError, TransactionSigned};
 
 /// https://github.com/MinaProtocol/mina/blob/985eda49bdfabc046ef9001d3c406e688bc7ec45/src/app/rosetta/lib/construction.ml#L786
 impl MinaMesh {
@@ -14,9 +15,45 @@ impl MinaMesh {
     let tx: TransactionSigned = TransactionSigned::from_json_string(&request.signed_transaction)
       .map_err(|_| MinaMeshError::JsonParse(Some("Failed to parse signed transaction".to_string())))?;
 
+    let signer_pk = self.extract_signer(&tx)?;
+
+    let user_command_payload = if let Some(payment) = &tx.payment {
+      let operations = generate_operations_user_command(payment);
+      self.validate_operations(&tx, &operations, payment.valid_until, payment.memo.clone())?
+    } else if let Some(stake_delegation) = &tx.stake_delegation {
+      let operations = generate_operations_user_command(stake_delegation);
+      self.validate_operations(&tx, &operations, stake_delegation.valid_until, stake_delegation.memo.clone())?
+    } else {
+      return Err(MinaMeshError::JsonParse(Some(
+        "Signed transaction must have one of: payment, stake_delegation".to_string(),
+      )));
+    };
+
+    tracing::debug!("signer_pk: {:?}", signer_pk);
+    tracing::debug!("User command payload: {:?}", user_command_payload);
+
     let tx_hash = self.hash_signed_transaction(&tx)?;
 
     Ok(TransactionIdentifierResponse::new(TransactionIdentifier::new(tx_hash)))
+  }
+
+  /// Extract and decompress the signer from the transaction.
+  fn extract_signer(&self, tx: &TransactionSigned) -> Result<PubKey, MinaMeshError> {
+    let source = &tx.get_source_address()?;
+    let pubkey = PubKey::from_address(source).map_err(|e| match e {
+      PubKeyError::AddressBase58
+      | PubKeyError::AddressLength
+      | PubKeyError::AddressRawByteLength
+      | PubKeyError::AddressChecksum => {
+        MinaMeshError::PublicKeyFormatNotValid(format!("Source address pk compression failed: {}", e))
+      }
+      PubKeyError::NonCurvePoint | PubKeyError::XCoordinate => {
+        MinaMeshError::PublicKeyFormatNotValid(format!("Source address pk decompression failed: {}", e))
+      }
+      _ => MinaMeshError::PublicKeyFormatNotValid("Source address not valid".into()),
+    })?;
+
+    Ok(pubkey)
   }
 
   fn hash_signed_transaction(&self, tx: &TransactionSigned) -> Result<String, MinaMeshError> {
